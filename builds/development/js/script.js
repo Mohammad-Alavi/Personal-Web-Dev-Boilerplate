6e3fd18c2b327a39cea1ede672fc66d8e39f73e5
Mohammad Alavi <mohammad.alavi1990@gmail.com>
2016-12-13 18:43:21 +0330

    Completed top-nav. Added horizontal-seperator mixin.
    Updated clock time.

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -15392,8 +15392,11 @@ $(document).ready(function() {
 	// Grab the current date
 	var currentDate = new Date();
 
-	// Set some date in the past. In this case, it's always been since Jan 1
-	var pastDate  = new Date(currentDate.getFullYear(), 0, 1);
+	// Set some date in the past.
+	// Real time is: 07/10/2016 08:00:00 am
+	var pastDate  = new Date(2016, 9, 7, 8, 0, 0);
+	// In this case, it's always been since Jan 1
+	//var pastDate = new Date(currentDate.getFullYear(), 0, 1);
 
 	// Calculate the difference in seconds between the future and current date
 	var diff = currentDate.getTime() / 1000 - pastDate.getTime() / 1000;
